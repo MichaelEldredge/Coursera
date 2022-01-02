c6e5bbc411792e7b9a37d48bed1d7f92596f8afc
MathEpic <michaelgeldredge@gmail.com>
2022-01-01 19:17:26 -0700

    Convert and save image

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,13 @@
 from PIL import Image
+from os import listdir
+import glob
 
-im = Image.open("img_bronze_rgba.gif")
+imagelist = listdir("images")
+for item in imagelist:
+    impath = "images/" + item
+    savename = impath[:-4] + ".jpg"
+    im = Image.open(impath)
+    newim = im.convert('RGB')
+    newim = newim.resize((600,400))
+    newim.save(savename)
 
-newim = im.convert('RGB')
-newim.show()
