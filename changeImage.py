2cc8baab6bc60fbc4029a9607567f6c23a4ffa60
MathEpic <michaelgeldredge@gmail.com>
2022-03-21 19:43:49 -0600

    Started working on the upload stage

--- changeImage.py
+++ changeImage.py
@@ -2,12 +2,13 @@ from PIL import Image
 from os import listdir
 import glob
 
-imagelist = listdir("images")
+imagelist = listdir("supplier-data/images")
 for item in imagelist:
-    impath = "images/" + item
-    savename = impath[:-4] + ".jpg"
-    im = Image.open(impath)
-    newim = im.convert('RGB')
-    newim = newim.resize((600,400))
-    newim.save(savename)
-
+    impath = "supplier-data/images/" + item
+    if impath[len(impath)-5 :] == ".tiff":
+        print("converting")
+        savename = impath[:-5] + ".jpeg"
+        im = Image.open(impath)
+        newim = im.convert('RGB')
+        newim = newim.resize((600,400))
+        newim.save(savename)
