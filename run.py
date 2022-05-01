7cd5cbe0db9c7a5d943491ccfc641c34870f597f
MathEpic <michaelgeldredge@gmail.com>
2022-04-30 21:10:33 -0600

    Attempted to fix run.py

--- run.py
+++ run.py
@@ -5,17 +5,21 @@ import requests
 import json
 import re
 
-lin_inst_ext = ""
+url = "http://localhost/upload/"
+lin_inst_ext = "35.232.216.221"
 post_address = "http://" + lin_inst_ext + "/fruits"
-desclist = os.listdir('Descriptions')
+desclist = os.listdir("supplier-data/descriptions")
 for desc in desclist:
     trimname = re.search("\w*",desc).group(0)
     print(trimname)
-    with open("Descriptions/" + trimname + ".txt") as f:
+    with open("supplier-data/descriptions/" + trimname + ".txt") as f:
         mylist = f.readlines()
         name = mylist[0]
         weight = int(re.search('[0-9]+' ,mylist[1] ).group(0))
         description = mylist[2]
         picname = trimname + ".jpeg"
-        m = json.dumps({'name':name, 'weight':weight, 'description':description, 'image_name':picname})
-        requests.post(post_address, m)
+        m = {'name':name, 'weight':weight, 'description':description, 'image_name':picname}
+        response = requests.post(url, json=m)
+        response.raise_for_status()
+        print(response.request.url)
+        print(response.status_code)
